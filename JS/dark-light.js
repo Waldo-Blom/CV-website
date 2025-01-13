ad67f6ac22aa5e1101af3d69e515df09c5f74b21
Waldo-Blom <waldoblom2004@gmail.com>
2025-01-13 13:48:07 +0200

    Darkmode set to default option

--- JS/dark-light.js
+++ JS/dark-light.js
@@ -1,6 +1,7 @@
 // check for saved 'darkMode' in localStorage
 let darkMode = localStorage.getItem('darkMode'); 
 
+
 const button = document.getElementById('dark-mode-toggle');
 const nav_ul = document.getElementById('nav-ul');
 const timeline_bg = document.getElementById('timeline-bg')
@@ -21,12 +22,11 @@ const enableDarkMode = () => {
 
 const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
-  localStorage.setItem('darkMode', null);
+  localStorage.setItem('darkMode', 'disabled');
 
   button.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" height="20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M120,40V16a8,8,0,0,1,16,0V40a8,8,0,0,1-16,0Zm72,88a64,64,0,1,1-64-64A64.07,64.07,0,0,1,192,128Zm-16,0a48,48,0,1,0-48,48A48.05,48.05,0,0,0,176,128ZM58.34,69.66A8,8,0,0,0,69.66,58.34l-16-16A8,8,0,0,0,42.34,53.66Zm0,116.68-16,16a8,8,0,0,0,11.32,11.32l16-16a8,8,0,0,0-11.32-11.32ZM192,72a8,8,0,0,0,5.66-2.34l16-16a8,8,0,0,0-11.32-11.32l-16,16A8,8,0,0,0,192,72Zm5.66,114.34a8,8,0,0,0-11.32,11.32l16,16a8,8,0,0,0,11.32-11.32ZM48,128a8,8,0,0,0-8-8H16a8,8,0,0,0,0,16H40A8,8,0,0,0,48,128Zm80,80a8,8,0,0,0-8,8v24a8,8,0,0,0,16,0V216A8,8,0,0,0,128,208Zm112-88H216a8,8,0,0,0,0,16h24a8,8,0,0,0,0-16Z"></path></svg>';
   nav_ul.classList.remove('blur-dark');
   nav_ul.classList.add('blur-light');
-
 }
  
 // If the user already visited and enabled darkMode
@@ -34,6 +34,7 @@ const disableDarkMode = () => {
 if (darkMode === 'enabled') {
   enableDarkMode();
 }
+
 
 // When someone clicks the button
 darkModeToggle.addEventListener('click', () => {
@@ -46,4 +47,14 @@ darkModeToggle.addEventListener('click', () => {
   } else {  
     disableDarkMode(); 
   }
+
 });
+
+document.addEventListener('DOMContentLoaded', () =>{
+ if (darkMode === null){
+  enableDarkMode();
+ }
+});
+
+
+
